0abd2c72a0ea4876f5e077134af83a659f4b91d5
Annon007 <nihal.abedin.annon99@gmail.com>
2022-01-12 15:22:42 +0600

    Chart

--- src/components/Chart/ChartBar.js
+++ src/components/Chart/ChartBar.js
@@ -3,7 +3,9 @@ import React from 'react';
 import './ChartBar.module.css';
 
 const ChartBar = (props) => {
+
   let barHeight="0%";
+  
   if(props.manValue>0){
     barHeight=`${Math.round((props.value*props.manValue)*100)}%`;
   }
